45067cf745fc824344129faef9a20aa18f55af24
Sean B. Higgins <higgisea@oregonstate.edu>
2017-12-07 13:42:10 -0800

    Adding the file paths in the server.

--- server.js
+++ server.js
@@ -4,7 +4,43 @@ var exphbs = require('express-handlebars');
 
 var pieData = require('./pieData');
 var app = express();
+var port = process.env.PORT || 3000;
+
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = process.env.MONGO_USER;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB;
+
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+
+app.get('/', function(req, res) {
+	res.status(200).render('home');
+});
+
+app.get('/pies', function(req, res) {
+	var pieDataCollection = mongoConnection.collection('pieData');
+	pieDataCollection.find({}).toArray(function (err, results) {
+		if (err) {
+			res.status(500).send("Error fetching pies from DB");
+		} else {
+			console.log("== query results:", results);
+			res.status(200).render('piePage', {
+				people: results
+			});
+		}
+	});
+});
+
+MongoClient.connect(mongoURL, function (err, connection) {
+  if (err) {
+    throw err;
+  }
+  mongoConnection = connection;
+  app.listen(port, function () {
+    console.log("== Server listening on port:", port);
+  });
+});
